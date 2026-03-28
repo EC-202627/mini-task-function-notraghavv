a9c34604717fb670bf58a70910421a40b5195c96
Raghav Pathak <raghav.pathak@avantika.edu.in>
2026-03-28 12:58:09 +0000

    Problem 02 fix

--- Problem_02.py
+++ Problem_02.py
@@ -5,7 +5,7 @@ def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
 
 book_title=input()
 days_overdue=int(input())
-daily_rate=int(input())
+daily_rate=float(input())
 fine=calculate_fine(book_title,days_overdue,daily_rate)
 print(f"Book: {book_title}")
 print(f"Days overdue: {days_overdue}")
